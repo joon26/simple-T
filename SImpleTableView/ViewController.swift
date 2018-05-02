0b8c5390c47c8032cceef1605355c2705e3cc6af
L703 <l703@L703ui-iMac-45.local>
2018-05-02 13:24:26 +0900

    SimpleTableVIew

--- SImpleTableView/ViewController.swift
+++ SImpleTableView/ViewController.swift
@@ -8,13 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    @IBOutlet weak var myTableView: UITableView!
+    var animals = ["오랑우탄","도웅화초","개미핡기","거들테일","아르마딜로"]
+    var country = ["일본","타이완","중국","한국","아프리카"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myTableView.delegate = self
+        myTableView.dataSource = self
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return animals.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //메모리 재활용
+        let identifer = "RE"
+        let cell = myTableView.dequeueReusableCell(withIdentifier: identifer, for: indexPath)
+        
+        cell.textLabel?.text = animals[indexPath.row]
+        cell.detailTextLabel?.text = country[indexPath.row]
+        
+        return cell
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
